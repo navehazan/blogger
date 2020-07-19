df1311fab2be26a9d4bbcbbe108793230a64c510
Nave Hazan <navehazan@Motis-Mac-mini.local>
2020-07-19 15:25:57 +0300

    fix emails

--- client/src/pages/selectBlogger.js
+++ client/src/pages/selectBlogger.js
@@ -17,14 +17,14 @@ const SelectBlogger = ({ bloggers }) => {
   ]);
 
   const sendEmail = async () => {
-    const emails = getEmails(state,bloggers);
+    const emails = getEmails(state, bloggers);
     try {
-       await axios.post("http://localhost:3001/bloggers", state);
-      setState([]);
-      console.log(state)
+      await axios.post("http://localhost:3001/bloggers", emails);
+      console.log(emails);
     } catch (e) {
       console.log(e);
     }
+    setState([]);
   };
 
   const toggleBlogger = (rowData) => {
