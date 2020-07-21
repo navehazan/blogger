5a784a0c3352be6ff03a88189abd1e721ac58206
Nave Hazan <navehazan@Motis-Mac-mini.local>
2020-07-21 12:41:22 +0300

    add serevr

---
+++ server/public/build/precache-manifest.82c307589185c0f91acbbfb0c05e1c97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1ba6f5b7a69adf6cf37e8ea5201eb0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b62996f495321f2cfe87",
+    "url": "/static/css/main.176b7693.chunk.css"
+  },
+  {
+    "revision": "9fe613027927c39e7838",
+    "url": "/static/js/2.5f296e8d.chunk.js"
+  },
+  {
+    "revision": "253d0d72fd1c6eb1e9606c0cb98dca11",
+    "url": "/static/js/2.5f296e8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b62996f495321f2cfe87",
+    "url": "/static/js/main.eedccedd.chunk.js"
+  },
+  {
+    "revision": "fb2902d34e7c1c4ff4b6",
+    "url": "/static/js/runtime-main.67fd0289.js"
+  }
+]);
